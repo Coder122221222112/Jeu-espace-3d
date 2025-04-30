a6604bf37fea8a41f305584019e771b705102bd5
Coder010203 <alphagm2912@gmail.com>
2025-04-30 17:38:42 -0400

    patch 10.0.03
    asteroid spawning

--- asteroid.js
+++ asteroid.js
@@ -1,6 +1,6 @@
 class Asteroid {
     constructor() {
-      this.x = random(-600, 600);
+      this.x = random(innerHeight, innerWidth);
       this.y = random(-600, 600);
       this.z = random(-3000, -2000);
       this.size = random(25, 50);
